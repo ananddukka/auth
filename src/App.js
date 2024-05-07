6c9a2c4093bc02b3414df0708c9b2bb86ac7dc0d
ananddukka <ananddukka83@gmail.com>
2024-05-07 16:53:32 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/about" component={About} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
